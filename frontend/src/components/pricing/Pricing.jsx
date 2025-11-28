6f4cfa133383acf9a1c461a47ad796466377077e
minhajdecryted <minhajwahid@decryptedlabs.io>
2025-11-28 20:05:43 +0500

    fixed image

--- frontend/src/components/pricing/Pricing.jsx
+++ frontend/src/components/pricing/Pricing.jsx
@@ -42,7 +42,7 @@ const Pricing = () => {
         <div className="max-w-7xl mx-auto p-2">
             <Heading size="large" title={"Simple, Transparent Pricing"} />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-14 bg-[url('/dna.png')] bg-cover bg-center bg-no-repeat">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-14 bg-[url('/layer.png')] bg-cover bg-center bg-no-repeat">
 
                 {plans.map((plan, idx) => (
                     <Card
